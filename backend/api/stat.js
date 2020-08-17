0f9b75785b4a150ca5268f025bca356768fe3c6d
maycon@oliveira <mayconbrito1998@hotmail.com>
2020-08-17 14:33:43 -0300

    create model mongodb

---
+++ backend/api/stat.js
@@ -0,0 +1,26 @@
+module.exports = app => {
+    const Stat = app.mongoose.model("Stat", {
+        users: Number,
+        categories: Number,
+        articles: Number,
+        createdAt: Date
+    })
+
+
+    const get = (req, res) => {
+        Stat.findOne({}, {}, { sort: { "createdAt": -1 } }) /* ({}= não vou filtar nada, {} = não vou selecionar nenhum atributo, {}= vou pegar de forma sort= ordenada a altima statistica armazenada no banco) */
+            .then(stat => {
+                const defaultStat = {
+                    users: 0,
+                    categories: 0,
+                    articles: 0
+                }
+
+                res.json(stat || defaultStat) /* se o banco tiver vazio me retorne os valres default  */
+            })
+
+    }
+
+
+    return { Stat, get }
+}
